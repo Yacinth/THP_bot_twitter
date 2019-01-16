c9e20a0351da34cf595fef5b3659e514f216f68d
Yacinth <fevriercjjc@gmail.com>
2019-01-16 18:44:21 +0100

    update hello world avec methode qui fontcionnent

---
+++ lib/bot_twitter.rb
@@ -0,0 +1,33 @@
+ # ligne très importante qui appelle la gem.
+require 'twitter'
+
+# n'oublie pas les lignes pour Dotenv ici…
+require 'dotenv'# Appelle la gem Dotenv
+
+Dotenv.load # Ceci appelle le fichier .env grâce à la gem Dotenv, et importe toutes les données enregistrées dans u
+
+
+# quelques lignes qui appellent les clés d'API de ton fichier .env
+def login_twitter
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+  if client == nil
+  return false # or return error
+  end
+  return client
+end
+
+def hello_world
+ 
+# ligne qui permet de tweeter sur ton compte
+client.update('@MSession7 Давай Давай')
+
+end
+
+#login_twitter
+hello_world
+
